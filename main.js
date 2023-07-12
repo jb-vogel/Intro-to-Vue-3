5810bf72bc4a13e15fd305b10727fd1b0c900428
johannes.borchard <johannes.borchard@vogel.de>
2023-07-12 15:27:32 +0200

    Lösung

--- main.js
+++ main.js
@@ -1,1 +1,12 @@
 const product = 'Socks'
+
+const app = Vue.createApp({
+  // data object
+  data() {
+    return {
+      // data item
+      product: "Boots",
+      description: "Laboris officia labore officia cupidatat. Non ut incididunt proident deserunt culpa quis elit ad quis irure irure elit qui. Enim aute ipsum in adipisicing commodo commodo."
+    }
+  }
+})
